17ca71360833f5355cd61e096d6e247695a94f60
Arnaud <arnaud.lafon@proton.me>
2024-03-10 20:44:59 +0100

    ask passwords

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module a-lafon/go-file-encrypt
 
 go 1.22.1
+
+require (
+	github.com/fatih/color v1.16.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/term v0.18.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
+	"log"
 	"os"
+
+	"github.com/fatih/color"
+	"golang.org/x/term"
 )
 
 func main() {
@@ -12,6 +18,73 @@ func main() {
 	}
 	command := os.Args[1]
 	fmt.Println(command)
+	switch command {
+	case "encrypt":
+		execEncrypt()
+	case "decrypt":
+		execDecrypt()
+	default:
+		printHelp()
+	}
+}
+
+func execEncrypt() {
+	if len(os.Args) < 3 {
+		log.Fatalln("file path must be specified. For more info run help command")
+	}
+
+	file := os.Args[2]
+
+	if !isValidFile(file) {
+		log.Fatalln("file not found")
+	}
+	fmt.Println(file)
+
+	password := getPassword()
+	fmt.Println(password)
+}
+
+func isValidFile(file string) bool {
+	if _, err := os.Stat(file); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+func getPassword() []byte {
+	fmt.Print("\nEnter password: ")
+	password, _ := term.ReadPassword(0)
+
+	isValid, err := isValidPassword(string(password))
+	if !isValid {
+		color.Yellow("\n%s\n", err)
+		return getPassword()
+	}
+
+	fmt.Print("\nConfirm password: ")
+	passwordConfirm, _ := term.ReadPassword(0)
+
+	if !isPasswordMatching(password, passwordConfirm) {
+		color.Red("\npasswords don't match. Please try again\n")
+		return getPassword()
+	}
+
+	return password
+}
+
+func isValidPassword(password string) (bool, error) {
+	if len(password) < 5 {
+		return false, errors.New("password must be at least 5 characters")
+	}
+	return true, nil
+}
+
+func isPasswordMatching(password []byte, password2 []byte) bool {
+	return bytes.Equal(password, password2)
+}
+
+func execDecrypt() {
+
 }
 
 func printHelp() {
